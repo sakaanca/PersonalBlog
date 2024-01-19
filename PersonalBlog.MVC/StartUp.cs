5d3e2682910c2cb82dcfe00f66bc7d78334ada59
M. Kaan SARICA <kaanmehmet2561@gmail.com>
2024-01-19 14:09:06 +0300

    .

---
+++ PersonalBlog.MVC/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Html;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace PersonalBlog.MVC
+{
+    public class StartUp
+    {
+       
+        public void ConfigureServices(IServiceCollection services)
+        {
+            sevices.AddControllersWithViews();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+            }
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                { 
+                    await context.Response.WriteAsync("Hello World !"); 
+                });
+            });
+        }
+        
+    }
+}
